4653585ddd09068fe360d6bfa1e6f214248164a0
Tima <tima148899@gmail.com>
2023-01-02 16:13:39 +0300

    fix bug

---
+++ Milk.java
@@ -0,0 +1,21 @@
+package com.example.playground.OOP;
+
+public class Milk extends Product {
+    public Milk(String name, double price, int fat) {
+        super(name, price);
+        this.setFat(fat);
+    }
+    private int fat;
+
+    public int getFat() {
+        return fat;
+    }
+
+    private void setFat(int fat) {
+        this.fat = fat;
+    }
+
+    public String toString(){
+        return String.format("%s, %d%% жирности", super.toString(), fat);
+    }
+}
---
+++ Soda.java
@@ -0,0 +1,23 @@
+package com.example.playground.OOP;
+
+import com.example.playground.OOP.Product;
+
+public class Soda extends Product {
+    public Soda(String name, double price, String flavor) {
+        super(name, price);
+        this.setFlavor(flavor);
+    }
+    private String flavor;
+
+    public String getFlavor() {
+        return flavor;
+    }
+
+    private void setFlavor(String flavor) {
+        this.flavor = flavor;
+    }
+
+    public String toString(){
+        return String.format("%s - %s", super.toString(), flavor);
+    }
+}
